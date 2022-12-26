c8ba02b9221d1563489fc97c6805b88f0e63f916
Oleg Charunov <charsyt@charsyt.ru>
2022-12-26 22:40:37 +0300

    main

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 print('Hello World')
 print('hellowrrrrr')
 print('hellowrrrrr')
-print('helorrrr')
-print('hellowrrrrr')
+
 print('hell;hjlkrrrrr')
-print('hellhjkl;rrrr')
-print('hejl;hudyrturrr')
-
-
 
